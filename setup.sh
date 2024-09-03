a5f4b32381ccb40f55fda1f5ccaf5a00103ca217
Angus Cummings <angus.cummings@csiro.au>
2024-09-03 00:47:04 +0000

    updates

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@
 # 1. Rocky Linux 8 installed
 # 2. nasei-openrvdas-config has been pulled
 
+# Grab the logged in username
+USER=$(logname)
+
 # Function to install Git
 install_packages() {
   if [ -x "$(command -v apt)" ]; then
@@ -33,11 +36,18 @@ git clone https://github.com/OceanDataTools/openrvdas.git $openrvdas_dir
 source $openrvdas_dir/utils/install_openrvdas.sh
 
 # Link the nasei config into /opt/openrvdas/local
-ln -s $script_dir $openrvdas_dir/local/nasei
+ln -fs $script_dir $openrvdas_dir/local/nasei
 chown -R rvdas:rvdas $openrvdas_dir/local/nasei 
+
+echo
+echo ###################################################
+echo
 
 URL="http://localhost/api/load-configuration-file/"
 DATA="{\"target_file\":\"$script_dir/config.yaml\"}"
+
+# Lazy wait to make sure the server has had a chance to start
+sleep 1
 
 echo "We are about to upload the config file for OpenRVDAS, but it requires HTTP authentication."
 echo "When prompted enter the password for OpenRVDAS configuration."
@@ -46,3 +56,6 @@ curl -v -u rvdas \
      $URL \
      -H "Content-Type: application/json" \
      -d $DATA
+
+
+sudo -u $USER setsid firefox localhost >/dev/null 2>&1 &
